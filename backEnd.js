ffcc7f2f7d04d0f9db5594f078d30ead894138a6
FU-yazEkip <115970659+FU-yazEkip@users.noreply.github.com>
2022-10-31 00:50:58 +0300

    Add files via upload

---
+++ backEnd.js
@@ -0,0 +1,16 @@
+function home() {
+    var element = document.getElementsByClassName("navList")[0];
+    alert("No Data in " + element.innerHTML);
+}
+function aboutUs(){
+    var element = document.getElementsByClassName("navList")[1];
+    alert("No Data in " + element.innerHTML);
+}
+function dataBase() {
+    var element = document.getElementsByClassName("navList")[2];
+    alert("No Data in " + element.innerHTML);
+}
+function sourceCode() {
+    var element = document.getElementsByClassName("navList")[3];
+    alert("No Data in " + element.innerHTML);
+}
